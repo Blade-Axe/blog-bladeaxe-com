f2d19cf14d1097a1d90edfde965b28e72c0b9f80
BladeAxe <Madaxam@protonmail.com>
2023-11-13 20:08:23 +0000

    updated text selection and background colours

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={` ${inter.className} bg-white text-black dark:bg-gray-900 dark:selection:bg-gray-900 dark:text-white h-full selection:bg-gray-50`}>
+      <body className={` ${inter.className} bg-slate-100 text-black dark:bg-gray-900 dark:selection:bg-gray-700 dark:text-white h-full selection:bg-gray-300 selection:text-teal-700 dark:selection:text-teal-400`}>
         <Providers>
           <Navbar />
           <main className='max-w-3xl mx-auto px-4 sm:px-6 lg:px-8'>{children}</main>
--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -31,7 +31,7 @@ export default async function SlugPage({params} : {params: {slug: string}}){
     };
 
     return(
-        <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+        <div className="xl:divide-y xl:divide-gray-300 xl:dark:divide-gray-700">
             <header className="pt-6 xl:pb-6">
                 <div className="space-y-1 text-center">
                     <div className="space-y-10">
